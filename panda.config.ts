550018f8bc93741beb01cebe497b054b944d2c33
kbrdn1 <kylianb1@icloud.com>
2024-05-03 11:23:46 +0200

    💄 feat: add styles vars + init Onest font

--- panda.config.ts
+++ panda.config.ts
@@ -5,14 +5,32 @@ export default defineConfig({
   preflight: true,
 
   // Where to look for your css declarations
-  include: ['./src/**/*.{ts,tsx,js,jsx,astro,vue}', './pages/**/*.{ts,tsx,js,jsx,astro,vue}'],
+  include: [
+    "./src/**/*.{ts,tsx,js,jsx,astro,vue}",
+    "./pages/**/*.{ts,tsx,js,jsx,astro,vue}",
+  ],
 
   // Files to exclude
   exclude: [],
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          black: { value: "#2D2520" },
+          white: { value: "#FAF8F0" },
+          olivine: { value: "#ADC178" },
+          teaGreen: { value: "#DDE5B6" },
+          parchment: { value: "#F0EAD2" },
+          umber: { value: "#6C584C" },
+          chamoisee: { value: "#A98467" },
+        },
+        fonts: {
+          body: { value: "Onest, system-ui, sans-serif" },
+        },
+      },
+    },
   },
 
   // The output directory for your css system
